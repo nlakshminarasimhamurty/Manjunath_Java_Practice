9332e1f31be55798d90871e2fbdbc5dd29050f54
nlakshminarasimhamurty <nlakshminarasimhamurty@gmail.com>
2021-09-03 19:06:50 +0530

    Variables code added

---
+++ src/test/java/com/learning/basics/First_Program.java
@@ -0,0 +1,27 @@
+package com.learning.basics;
+
+public class First_Program {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hi Naveen");
+
+		System.out.println(1 + 2); // 3
+
+		System.out.println("hi Naveen" + " 4 + 5 "); // concatination //
+
+		System.out.println(2 + 3 + " Naveen");
+
+		System.out.print("Hi ");
+
+		System.out.println("HI Naveen " + (4 + 5));
+
+		System.out.println("hi \n naveen");
+
+		System.out.println("hi \t naveen");
+		
+		//System.out.print();
+
+	}
+
+}
---
+++ src/test/java/com/learning/basics/Variable1.java
@@ -0,0 +1,33 @@
+package com.learning.basics;
+
+public class Variable1 {
+
+	public static void main(String[] args) {
+
+		int i = 10; // [15]
+		System.out.println(i);
+		int j = 8000;
+		j = 9000;
+		System.out.println("J Value = " + j);
+		i = 15;
+		i = 500;
+		i = 1000;
+
+		i = 10000;
+		i = 5000;
+
+		System.out.println(i);
+		System.out.println(i);
+		System.out.println(i);
+		System.out.println(i);
+		System.out.println(i);
+		System.out.println(i);
+		System.out.println(i);
+
+		j = i;
+
+		System.out.println("J value is " + j);
+
+	}
+
+}
